1e44c1e0ef937f373892e885888ca8bfa393a840
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-02-26 09:28:51 +0700

    3.4 Percobaan 3 Matakuliah

---
+++ Praktikum03/Matakuliah24.java
@@ -0,0 +1,15 @@
+package Praktikum03;
+
+public class Matakuliah24 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah24(String kode, String nama, int sks, int jumlahJam){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
