1fd7cfcea7de602c8a14bea30bcca91e3c383966
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-02-26 10:39:54 +0700

    3.4 Percobaan 3 Pertanyaan No 4

--- Praktikum03/MatakuliahDemo24.java
+++ Praktikum03/MatakuliahDemo24.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class MatakuliahDemo24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah24[] arrayOfMatakuliah24 = new Matakuliah24[3];
+        System.out.print("Masukkan jumlah matakuliah : ");
+        int jumlah = Integer.parseInt(sc.nextLine());
+        System.out.println();
+        
+        Matakuliah24[] arrayOfMatakuliah24 = new Matakuliah24[jumlah];
         String kode, nama, dummy;
         int sks, jumlahJam;
 
