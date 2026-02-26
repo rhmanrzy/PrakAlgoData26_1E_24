f0e0f5797d0c76e0af8b33c3a23fde9012af52ee
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-02-26 10:03:27 +0700

    3.4 Percobaan 3 Pertanyaan No 2 Matakuliah

--- Praktikum03/Matakuliah24.java
+++ Praktikum03/Matakuliah24.java
@@ -6,7 +6,18 @@ public class Matakuliah24 {
     public int sks;
     public int jumlahJam;
 
+    public Matakuliah24(){
+
+    }
+
     public Matakuliah24(String kode, String nama, int sks, int jumlahJam){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    void tambahData(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
