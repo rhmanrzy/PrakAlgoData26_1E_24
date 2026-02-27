fcafebbdd2a9cf06f253fa891776cba315001761
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-02-27 23:02:16 +0700

    3,5 Tugas 2 Data Dosen

--- Praktikum03/DataDosen24.java
+++ Praktikum03/DataDosen24.java
@@ -1,15 +1,15 @@
 package Praktikum03;
 
 public class DataDosen24 {
-    void dataSemuaDosen(Dosen24[] arrayOfDosen24s){
-        for (int i = 0; i < arrayOfDosen24s.length; i++) {
-            arrayOfDosen24s[i].cetakInfo();
+    void dataSemuaDosen(Dosen24[] arrayOfDosen24){
+        for (int i = 0; i < arrayOfDosen24.length; i++) {
+            arrayOfDosen24[i].cetakInfo();
         }
     }
 
-    void jumlahDosenPerJenisKelamin(Dosen24[] arrayOfDosen24s){
+    void jumlahDosenPerJenisKelamin(Dosen24[] arrayOfDosen24){
         int pria = 0, wanita = 0;
-        for (Dosen24 d : arrayOfDosen24s) {
+        for (Dosen24 d : arrayOfDosen24) {
             if (d.jenisKelamin) {
                 pria++;
             } else {
@@ -22,9 +22,9 @@ public class DataDosen24 {
         System.out.println("------------------------------");
     }
 
-    void rerataUsiaDosenPerJenisKelamin(Dosen24[] arrayOfDosen24s){
+    void rerataUsiaDosenPerJenisKelamin(Dosen24[] arrayOfDosen24){
         int totalPria = 0, totalWanita = 0, jumlahPria = 0, jumlahWanita = 0;
-        for (Dosen24 d : arrayOfDosen24s) {
+        for (Dosen24 d : arrayOfDosen24) {
             if (d.jenisKelamin) {
                 totalPria += d.usia;
                 jumlahPria++;
@@ -43,9 +43,9 @@ public class DataDosen24 {
         }
     }
 
-    void infoDosenPalingTua(Dosen24[] arrayOfDosen24s){
-        Dosen24 tertua = arrayOfDosen24s[0];
-        for (Dosen24 d : arrayOfDosen24s) {
+    void infoDosenPalingTua(Dosen24[] arrayOfDosen24){
+        Dosen24 tertua = arrayOfDosen24[0];
+        for (Dosen24 d : arrayOfDosen24) {
             if (d.usia > tertua.usia) {
                 tertua = d;
             }
@@ -55,10 +55,10 @@ public class DataDosen24 {
         tertua.cetakInfo();
     }
 
-    void infoDosenPalingMuda(Dosen24[] arrayOfDosen24s){
-        Dosen24 termuda = arrayOfDosen24s[0];
-        for (Dosen24 d : arrayOfDosen24s) {
-            if (d.usia > termuda.usia) {
+    void infoDosenPalingMuda(Dosen24[] arrayOfDosen24){
+        Dosen24 termuda = arrayOfDosen24[0];
+        for (Dosen24 d : arrayOfDosen24) {
+            if (d.usia < termuda.usia) {
                 termuda = d;
             }
         }
