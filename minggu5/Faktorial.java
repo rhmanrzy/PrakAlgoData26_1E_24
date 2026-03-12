e343410daf523900d24a2d7f2ed524448c9f1f67
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-03-12 08:46:56 +0700

    5.2 Percobaan 1 Pertanyaan No 2

--- minggu5/Faktorial.java
+++ minggu5/Faktorial.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class Faktorial{
     int faktorialBF(int n){
         int fakto = 1;
-        for(int i = 1; i <= n; i++){
+        int i = 1;
+        while(i <= n){
             fakto = fakto * i;
+            i++;
         }
         return fakto;
     }
