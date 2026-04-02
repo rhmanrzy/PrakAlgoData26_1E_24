9214331725f24b80b347e640eca8701f2fba7314
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-04-02 08:11:13 +0700

    5.2 Percobaan 1 SortingMain

---
+++ Praktikum05/SortingMain24.java
@@ -0,0 +1,29 @@
+package Praktikum05;
+
+public class SortingMain24 {
+    public static void main(String[] args) {
+        int a[] = {20, 10, 2, 7, 12};
+        Sorting24 dataurut1 = new Sorting24(a, a.length);
+        System.out.println("Data awal 1");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        dataurut1.tampil();
+
+        int b[] = {30, 20, 2, 8, 14};
+        Sorting24 dataurut2 = new Sorting24(b, b.length);
+        System.out.println("Data awal 2");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        dataurut1.tampil();
+
+        int c[] = {40, 10, 4, 9, 3};
+        Sorting24 dataurut3 = new Sorting24(b, b.length);
+        System.out.println("Data awal 3");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        dataurut1.tampil();
+    }
+}
