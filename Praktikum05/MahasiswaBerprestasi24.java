023f4a0f53001a0f30e18e99da4a61c17a51c90b
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-04-02 13:11:32 +0700

    5.4 Percobaan 3 Pertanyaan

--- Praktikum05/MahasiswaBerprestasi24.java
+++ Praktikum05/MahasiswaBerprestasi24.java
@@ -51,10 +51,10 @@ public class MahasiswaBerprestasi24 {
     }
 
     void insertionSort(){
-        for (int i = 1; i < listMhs.length; i++) {
+        for (int i = 1; i < idx; i++) {
             Mahasiswa24 temp = listMhs[i];
             int j = i;
-            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
                 listMhs[j] = listMhs[j - 1];
                 j--;
             }
