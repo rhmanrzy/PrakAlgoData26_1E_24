37336c3904d6a6397a93a5c63f4ffee8b7fb147f
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-04-02 09:46:14 +0700

    5.3 Percobaan 2 MahasiswaBerprestasi

---
+++ Praktikum05/MahasiswaBerprestasi24.java
@@ -0,0 +1,34 @@
+package Praktikum05;
+
+public class MahasiswaBerprestasi24 {
+    Mahasiswa24[] listMhs = new Mahasiswa24[5];
+    int idx = 0;
+
+    void tambah(Mahasiswa24 m){
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else{
+            System.out.println("Data sudah penuh");
+        }
+    }
+
+    void tampil(){
+        for (int i = 0; i < idx; i++){
+            listMhs[i].tampilInformasi();
+            System.out.println("---------------------------------");
+        }
+    }
+
+    void bubbleSort(){
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 1; j < idx - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa24 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
+}
