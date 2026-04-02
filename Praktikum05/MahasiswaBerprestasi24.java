95338ec78792e333287622e8ebc0c7d5ee020c47
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-04-02 12:51:15 +0700

    5.4 Percobaan 3 Mahasiswa Berprestasi

--- Praktikum05/MahasiswaBerprestasi24.java
+++ Praktikum05/MahasiswaBerprestasi24.java
@@ -49,4 +49,16 @@ public class MahasiswaBerprestasi24 {
             listMhs[i] = tmp;
         }
     }
+
+    void insertionSort(){
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa24 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = temp;
+        }
+    }
 }
