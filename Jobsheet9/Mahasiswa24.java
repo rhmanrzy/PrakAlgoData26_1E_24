065e7fe1850529cbba41801eed31fb5dd7d1e131
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-04-23 08:38:06 +0700

    P9 Percobaan 1 Stack Tugas Mahasiswa

---
+++ Jobsheet9/Mahasiswa24.java
@@ -0,0 +1,19 @@
+package Jobsheet9;
+
+public class Mahasiswa24 {
+    String nama;
+    String nim;
+    String kelas;
+    int nilai;
+
+    Mahasiswa24(String nama, String nim, String kelas){
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai){
+        this.nilai = nilai;
+    }
+}
