dd23f10dc75e2c83040b0d533b5d64ba0dff91e8
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-04-23 10:16:54 +0700

    p( Percobaan 2 Mahasiswa Demo

--- Jobsheet9/MahasiswaDemo24.java
+++ Jobsheet9/MahasiswaDemo24.java
@@ -45,6 +45,8 @@ public class MahasiswaDemo24 {
                         dinilai.tugasDinilai(nilai);
 
                         System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                        String biner = stack.konversiDesimalKeBiner(nilai);
+                        System.out.println("Nilai Biner Tugas : " + biner);
                     }
                     break;
 
