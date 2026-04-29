93dfd6b1b8571da2b5b334291b0381a33bbe3753
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-04-29 17:19:24 +0700

    P10 Percobaan 2 Pertanyaan Layanan Akademik SIAKAD

--- P2Jobsheet10/LayananAkademikSIAKAD.java
+++ P2Jobsheet10/LayananAkademikSIAKAD.java
@@ -15,6 +15,7 @@ public class LayananAkademikSIAKAD {
             System.out.println("3. Lihat Mahasiswa Terdepan");
             System.out.println("4. Lihat Semua Antrian");
             System.out.println("5. Jumlah Mahasiswa dalam Antrian");
+            System.out.println("6. Cek Antrian paling belakang");
             System.out.println("0. Keluar");
             System.out.print("Pilih menu : ");
             pilihan = sc.nextInt();
@@ -52,6 +53,10 @@ public class LayananAkademikSIAKAD {
 
                 case 5:
                     System.out.println("Jumlah dalam antrian : " + antrian.getJumlahAntrian());
+                    break;
+
+                case 6:
+                    antrian.LihatAkhir();
                     break;
 
                 case 0:
