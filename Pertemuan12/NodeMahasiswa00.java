075d0c1d51867e987957ced10a1935b7c71823c1
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-07 08:35:32 +0700

    P11 Percobaan 1 Node Mahasiswa

---
+++ Pertemuan12/NodeMahasiswa00.java
@@ -0,0 +1,11 @@
+package Pertemuan12;
+
+public class NodeMahasiswa00 {
+    Mahasiswa00 data;
+    NodeMahasiswa00 next;
+
+    public NodeMahasiswa00(Mahasiswa00 data, NodeMahasiswa00 next) {
+        this.data = data;
+        this.next = next;
+    }
+}
