9920a7aa56f27e6000ca1bd620e958f1a2678d61
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-09 14:28:23 +0700

    P11 Percobaan 1 Pertanyaan No 3

--- Pertemuan12/SLLMain00.java
+++ Pertemuan12/SLLMain00.java
@@ -1,29 +1,34 @@
 package Pertemuan12;
 
+import java.util.Scanner;
+
 public class SLLMain00 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         SingleLinkedList00 sll = new SingleLinkedList00();
 
-        Mahasiswa00 mhs1 = new Mahasiswa00("2201", "Budi", "SIB-1B", 3.8);
-        Mahasiswa00 mhs2 = new Mahasiswa00("2202", "Lia", "TI-1E", 3.9);
-        Mahasiswa00 mhs3 = new Mahasiswa00("2203", "Raka", "SIB-1A", 3.5);
-        Mahasiswa00 mhs4 = new Mahasiswa00("2204", "Rahma", "TI-1B", 3.8);
+        int jumlah;
+        System.out.print("Masukkan jumlah data : ");
+        jumlah = sc.nextInt();
+        sc.nextLine();
 
-        sll.addLast(mhs1);
-        sll.addLast(mhs2);
-        sll.addLast(mhs3);
-        sll.addLast(mhs4);
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
 
-        System.out.println("data index 1 : ");
-        sll.getData(1);
-
-        System.out.println("data mahasiswa an Raka berada pada index : " + sll.indexOf("Raka"));
-        System.out.println();
-
-        sll.removeFirst();
-        sll.removeLast();
-        sll.print();
-        sll.removeAt(0);
+            Mahasiswa00 mhs = new Mahasiswa00(nim, nama, kelas, ipk);
+            sll.addLast(mhs);
+        }
+        System.out.println("Isi Linked List");
         sll.print();
     }
 }
