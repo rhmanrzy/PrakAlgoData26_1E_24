058bc5ac8a32eb89614ee0bf67ec86e9a6a01bd4
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-09 15:41:39 +0700

    P11 Tugas Node

---
+++ Pertemuan12/AntrianLayananUnitKemahasiswaan/Node24.java
@@ -0,0 +1,11 @@
+package Pertemuan12.AntrianLayananUnitKemahasiswaan;
+
+public class Node24 {
+    Mahasiswa24 data;
+    Node24 next;
+
+    public Node24(Mahasiswa24 data) {
+        this.data = data;
+        this.next = null;
+    }
+}
