9470272f3651dd134c0fc52bf5650b115506c3ae
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-11 19:13:27 +0700

    P12 Percobaan 1 Double Linked List

---
+++ Jobsheet12/DoubleLinkedList24.java
@@ -0,0 +1,77 @@
+package Jobsheet12;
+
+import org.w3c.dom.Node;
+
+public class DoubleLinkedList24 {
+    Node24 head;
+    Node24 tail;
+
+    public DoubleLinkedList24() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(Mahasiswa24 data) {
+        Node24 newNode = new Node24(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    public void addLast(Mahasiswa24 data) {
+        Node24 newNode = new Node24(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void inserAfter(String keyNim, Mahasiswa24 data) {
+        Node24 current = head;
+        while (current != null && !current.data.nim.equals(keyNim)) {
+            current = current.next;
+        }
+        if (current == null) {
+            System.out.println("Data dengan NIM " + keyNim + " tidak ditemukan.");
+            return;
+        }
+
+        Node24 newNode = new Node24(data);
+
+        if (current == tail) {
+            newNode.prev = current;
+            current.next = newNode;
+            tail = newNode;
+        } else {
+            newNode.prev = current;
+            newNode.next = current.next;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+        System.out.println("Data berhasil disisipkan setelah NIM " + keyNim);
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Linked List masih kosong.");
+            return;
+        }
+
+        Node24 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;
+        }
+    }
+}
