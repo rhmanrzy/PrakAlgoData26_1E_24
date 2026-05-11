0360228d344ec575b2ba18052bed69e2c1f94670
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-11 19:13:00 +0700

    P12 Percobaan 1 Node

---
+++ Jobsheet12/Node24.java
@@ -0,0 +1,15 @@
+package Jobsheet12;
+
+import org.w3c.dom.Node;
+
+public class Node24 {
+    Mahasiswa24 data;
+    Node24 prev;
+    Node24 next;
+
+    public Node24(Mahasiswa24 data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
