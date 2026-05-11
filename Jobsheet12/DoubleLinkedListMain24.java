85c864c9c7f197d653d8a6eed7a96df5a1f56a99
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-11 20:17:48 +0700

    P12 Percobaan 1 Pertanyaan No 6 Main

--- Jobsheet12/DoubleLinkedListMain24.java
+++ Jobsheet12/DoubleLinkedListMain24.java
@@ -26,6 +26,7 @@ public class DoubleLinkedListMain24 {
             System.out.println("4. Hapus data di awal");
             System.out.println("5. Hapus data di akhir");
             System.out.println("6. Tampilkan data");
+            System.out.println("7. Tampilkan data terbalik");
             System.out.println("0. Keluar");
             System.out.print("Pilih menu : ");
             pilihan = scan.nextInt();
@@ -57,6 +58,9 @@ public class DoubleLinkedListMain24 {
                     break;
                 case 6:
                     list.print();
+                    break;
+                case 7:
+                    list.printReverse();
                     break;
                 case 0:
                     System.out.println("Program selesai.");
