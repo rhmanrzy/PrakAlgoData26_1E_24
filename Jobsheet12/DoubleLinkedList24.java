29214b14ffb22a78da8eca2cd76e2d0ea9854724
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-11 20:17:23 +0700

    P12 Percobaan 1 Pertanyaan No 6

--- Jobsheet12/DoubleLinkedList24.java
+++ Jobsheet12/DoubleLinkedList24.java
@@ -99,4 +99,16 @@ public class DoubleLinkedList24 {
             tail.next = null;
         }
     }
+
+    public void printReverse() {
+        if (isEmpty()) {
+            System.out.println("Linked List masih kosong.");
+            return;
+        }
+        Node24 current = tail;
+        while (current != null) {
+            current.data.tampil();
+            current = current.prev;
+        }
+    }
 }
