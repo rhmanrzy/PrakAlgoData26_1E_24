a450af0e9d27e27f0114f127d334419fa72417b0
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-11 21:56:14 +0700

    P12 Tugas Main

--- Jobsheet12/DoubleLinkedListMain24.java
+++ Jobsheet12/DoubleLinkedListMain24.java
@@ -27,7 +27,14 @@ public class DoubleLinkedListMain24 {
             System.out.println("4. Hapus data di awal");
             System.out.println("5. Hapus data di akhir");
             System.out.println("6. Tampilkan data");
-            System.out.println("7. Tampilkan data terbalik");
+            System.out.println("7. Hapus data setelah NIM tertentu");
+            System.out.println("8. Hapus data berdasarkan index");
+            System.out.println("9. Tampilkan data");
+            System.out.println("10. Tampilkan data terbalik");
+            System.out.println("11. Tampilkan data pertama");
+            System.out.println("12. Tampilkan data terakhir");
+            System.out.println("13. Tampilkan data berdasarkan index");
+            System.out.println("14. Tampilkan jumlah data");
             System.out.println("0. Keluar");
             System.out.print("Pilih menu : ");
             pilihan = scan.nextInt();
@@ -35,10 +42,12 @@ public class DoubleLinkedListMain24 {
 
             switch (pilihan) {
                 case 1:
+                    System.out.println("Masukkan data mahasiswa");
                     Mahasiswa24 mhsAwal = inputMahasiswa(scan);
                     list.addFirst(mhsAwal);
                     break;
                 case 2:
+                    System.out.println("Masukkan data mahasiswa");
                     Mahasiswa24 mhsAkhir = inputMahasiswa(scan);
                     list.addLast(mhsAkhir);
                     break;
@@ -47,20 +56,56 @@ public class DoubleLinkedListMain24 {
                     String keyNim = scan.nextLine();
                     System.out.println("Masukkan data baru : ");
                     Mahasiswa24 dataBaru = inputMahasiswa(scan);
-                    list.inserAfter(keyNim, dataBaru);
+                    list.insertAfter(keyNim, dataBaru);
                     break;
                 case 4:
-                    list.removeFirst();
+                    System.out.print("Masukkan index : ");
+                    int indexTambah = scan.nextInt();
+                    scan.nextLine();
+                    System.out.println("Masukkan data mahasiswa : ");
+                    Mahasiswa24 dataIndex = inputMahasiswa(scan);
+                    list.add(indexTambah, dataIndex);
                     break;
                 case 5:
-                    list.removeLast();
+                    list.removeFirst();
                     break;
                 case 6:
-                    list.print();
+                    list.removeLast();
                     break;
                 case 7:
+                    System.out.print("Masukkan NIM yang dicari : ");
+                    String nimHapus = scan.nextLine();
+                    list.removeAfter(nimHapus);
+                    break;
+                case 8:
+                    System.out.print("Masukkan index yang ingin dihapus : ");
+                    int indexHapus = scan.nextInt();
+                    scan.nextLine();
+                    list.remove(indexHapus);
+                    break;
+                case 9:
+                    System.out.println("Isi linked list : ");
+                    list.print();
+                    break;
+                case 10:
+                    System.out.println("Isi linked list reverse : ");
                     list.printReverse();
                     break;
+                case 11:
+                    list.getFirst();
+                    break;
+                case 12:
+                    list.getLast();
+                    break;
+                case 13:
+                    System.out.print("Masukkan index : ");
+                    int indexCari = scan.nextInt();
+                    scan.nextLine();
+                    list.getIndex(indexCari);
+                    break;
+                case 14:
+                    System.out.println("Jumlah data : " + list.size());
+                    break;  
                 case 0:
                     System.out.println("Program selesai.");
                     break;
