e9be4d9ce58c92f2bf636d52ab6ca520d4926eb4
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-21 21:14:17 +0700

    CM Node Pembeli

---
+++ CM2/NodePembeli24.java
@@ -0,0 +1,13 @@
+package CM2;
+
+public class NodePembeli24 {
+    NodePembeli24 prev24;
+    Pembeli24 data24;
+    NodePembeli24 next24;
+
+    public NodePembeli24(NodePembeli24 prev24, Pembeli24 data24, NodePembeli24 next24) {
+        this.prev24 = prev24;
+        this.data24 = data24;
+        this.next24 = next24;
+    }
+}
