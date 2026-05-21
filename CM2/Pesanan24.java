f82ac1b745b30bb2a95297876c96c52a5e12a320
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-21 21:13:24 +0700

    CM Pesanan

---
+++ CM2/Pesanan24.java
@@ -0,0 +1,13 @@
+package CM2;
+
+public class Pesanan24 {
+    int kodePesanan24;
+    String namaPesanan24;
+    int harga24;
+
+    public Pesanan24(int kodePesanan24, String namaPesanan24, int harga24) {
+        this.kodePesanan24 = kodePesanan24;
+        this.namaPesanan24 = namaPesanan24;
+        this.harga24 = harga24;
+    }
+}
