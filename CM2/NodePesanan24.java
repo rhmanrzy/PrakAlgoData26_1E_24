613d0278c37b0e12e9d211484fcd455e162bb860
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-21 21:14:02 +0700

    CM Node Pesanan

---
+++ CM2/NodePesanan24.java
@@ -0,0 +1,13 @@
+package CM2;
+
+public class NodePesanan24 {
+    NodePesanan24 prev24;
+    Pesanan24 data24;
+    NodePesanan24 next24;
+
+    public NodePesanan24(NodePesanan24 prev24, Pesanan24 data24, NodePesanan24 next24) {
+        this.prev24 = prev24;
+        this.data24 = data24;
+        this.next24 = next24;
+    }
+}
