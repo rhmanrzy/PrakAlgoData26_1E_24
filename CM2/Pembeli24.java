428dab9857b3fb1b2992b88b070cae7b1d1673bb
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-05-21 21:13:44 +0700

    CM Pembeli

---
+++ CM2/Pembeli24.java
@@ -0,0 +1,13 @@
+package CM2;
+
+public class Pembeli24 {
+    int noAntrian24;
+    String namaPembeli24;
+    String noHp24;
+
+    public Pembeli24(int noAntrian24, String namaPembeli24, String noHp24) {
+        this.noAntrian24 = noAntrian24;
+        this.namaPembeli24 = namaPembeli24;
+        this.noHp24 = noHp24;
+    }
+}
