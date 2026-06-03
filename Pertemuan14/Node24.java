32a23e05387488bc3a7d44d502d9a2bd0eccfda3
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-06-03 12:59:17 +0700

    P14 Percobaan 1 Node

---
+++ Pertemuan14/Node24.java
@@ -0,0 +1,14 @@
+package Pertemuan14;
+
+public class Node24 {
+    Mahasiswa24 mahasiswa24;
+    Node24 left24, right24;
+
+    public Node24() {
+    }
+
+    public Node24(Mahasiswa24 mahasiswa24) {
+        this.mahasiswa24 = mahasiswa24;
+        left24 = right24 = null;
+    }
+}
