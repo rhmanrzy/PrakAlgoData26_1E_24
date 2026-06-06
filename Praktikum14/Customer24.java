97f3c69cdb07f379fefce740d931a3604dc2c34d
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-06-06 13:24:01 +0700

    P15 13.2 Customer

---
+++ Praktikum14/Customer24.java
@@ -0,0 +1,19 @@
+package Praktikum14;
+
+public class Customer24 {
+    public int id24;
+    public String name24;
+
+    public Customer24() {
+        
+    }
+
+    public Customer24(int id24, String name24) {
+        this.id24 = id24;
+        this.name24 = name24;
+    }
+
+    public String toString() {
+        return " ID: " + this.id24 + " Nama: " + this.name24;
+    }
+}
