0540fa4f3ad4c36ba8f939892a663aa67a456939
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-06-06 13:24:22 +0700

    P15 13.2 Book

---
+++ Praktikum14/Book24.java
@@ -0,0 +1,19 @@
+package Praktikum14;
+
+public class Book24 {
+    public String isbn24;
+    public String title24;
+
+    public Book24() {
+        
+    }
+
+    public Book24(String isbn24, String title24) {
+        this.isbn24 = isbn24;
+        this.title24 = title24;
+    }
+
+    public String toString() {
+        return " ISBN: " + this.isbn24 + " Title: " + this.title24;
+    }
+}
