be88b01450ebef355a3f7b5500b200019eb576b2
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-06-16 18:10:05 +0700

    Percobaan 1 Pertanyaan No 2

--- P16/ContohList.java
+++ P16/ContohList.java
@@ -7,15 +7,15 @@ import java.util.List;
 public class ContohList {
     public static void main(String[] args) {
 
-        LinkedList<String> l = new LinkedList<>();
-        l.add("Mi goreng");
-        l.add("Nasi Goreng");
-        l.add("Mi ayam");
-        l.add("Cireng");
+        List<Integer> l = new ArrayList();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
 
         System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n",
                 l.get(0), l.size(), l.get(l.size() - 1));
-        l.add("Cireng");
+        l.add(4);
         l.remove(0);
         System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n",
                 l.get(0), l.size(), l.get(l.size() - 1));
