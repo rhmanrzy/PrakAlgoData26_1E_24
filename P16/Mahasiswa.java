ac2f2a2705afedc1cade56c87e080779b240f2fc
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2026-06-16 19:57:23 +0700

    Percobaan 3

---
+++ P16/Mahasiswa.java
@@ -0,0 +1,19 @@
+package P16;
+
+public class Mahasiswa {
+    String nim, nama, notelp;
+
+    public Mahasiswa() {
+    }
+
+    public Mahasiswa(String nim, String nama, String notelp) {
+        this.nim = nim;
+        this.nama = nama;
+        this.notelp = notelp;
+    }
+
+    @Override
+    public String toString() {
+        return "Mahasiswa{" + "nim=" + ", nama=" + nama + ", notelp=" + notelp + '}';
+    }
+}
